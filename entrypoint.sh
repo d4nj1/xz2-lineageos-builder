11a40585b30d3f73c37845d26d982c4d0e3449ca
Daniel <code@devmind.org>
2020-07-14 13:34:29 +0200

    * Fix wrong cache source folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,7 +49,7 @@ sed -i 's|read -n1 -r -p "Press space to continue..."||g' ~/SCRIPTS_BUILD/ROM/SO
 
 # Set build cache folder to customROM out dir
 
-sed -i 's|/media/martin/extLinux/developer/android/out/lineageOS/10|/home/developer/android/rom/lineageOS/10/out|g' ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
+sed -i 's|/media/martin/extLinux/developer/android/cache/lineageOS/10|/home/developer/android/rom/lineageOS/10/out|g' ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
 
 # Run build
 
