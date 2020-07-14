74247cddaf78db3312841e3036e15fc557901d85
Daniel <code@devmind.org>
2020-07-14 22:34:25 +0200

    * Override build out folder * Added debug build.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,56 +1,62 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -z "$1" ]; then
-	lineageBranch="lineage-17.1"
-else
-	lineageBranch=$1
-fi
+(
+  if [ -z "$1" ]; then
+    lineageBranch="lineage-17.1"
+  else
+    lineageBranch=$1
+  fi
 
-if [ -z "$2" ]; then
-	scriptsBranch="android-10"
-else
-	scriptsBranch=$2
-fi
+  if [ -z "$2" ]; then
+    scriptsBranch="android-10"
+  else
+    scriptsBranch=$2
+  fi
 
-# Fetch LineageOS
+  # Fetch LineageOS
 
-if [ ! -d ~/android/rom/lineageOS/10 ]; then
-	mkdir -p ~/android/rom/lineageOS/10
-	cd ~/android/rom/lineageOS/10
-	repo init -u git://github.com/LineageOS/android.git -b $lineageBranch
-else
-  cd ~/android/rom/lineageOS/10
-  repo forall -vc "git reset --hard"
-fi
+  if [ ! -d ~/android/rom/lineageOS/10 ]; then
+    mkdir -p ~/android/rom/lineageOS/10
+    cd ~/android/rom/lineageOS/10
+    repo init -u git://github.com/LineageOS/android.git -b $lineageBranch
+  else
+    cd ~/android/rom/lineageOS/10
+    repo forall -vc "git reset --hard"
+  fi
 
-# Fetch patched manifest
+  # Fetch patched manifest
 
-if [ ! -d ~/android/rom/lineageOS/10/.repo/local_manifests ]; then
-	git clone https://github.com/MartinX3-AndroidDevelopment-LineageOS/local_manifests.git -b MartinX3/$lineageBranch ~/android/rom/lineageOS/10/.repo/local_manifests
-fi
+  if [ ! -d ~/android/rom/lineageOS/10/.repo/local_manifests ]; then
+    git clone https://github.com/MartinX3-AndroidDevelopment-LineageOS/local_manifests.git -b MartinX3/$lineageBranch ~/android/rom/lineageOS/10/.repo/local_manifests
+  fi
 
-# Fetch build scripts
+  # Fetch build scripts
 
-if [ ! -d ~/SCRIPTS_BUILD ]; then
-  git clone https://github.com/MartinX3-AndroidDevelopment/SCRIPTS_BUILD.git -b $scriptsBranch ~/SCRIPTS_BUILD
-else
-	cd ~/SCRIPTS_BUILD
-	git reset --hard
-	git pull
-fi
+  if [ ! -d ~/SCRIPTS_BUILD ]; then
+    git clone https://github.com/MartinX3-AndroidDevelopment/SCRIPTS_BUILD.git -b $scriptsBranch ~/SCRIPTS_BUILD
+  else
+    cd ~/SCRIPTS_BUILD
+    git reset --hard
+    git pull
+  fi
 
-chmod +x ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
-cd ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/
+  chmod +x ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
+  cd ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/
 
-# Remove interactive input
+  # Remove interactive input
 
-sed -i 's|read -n1 -r -p "Press space to continue..."||g' ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
+  sed -i 's|read -n1 -r -p "Press space to continue..."||g' ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
 
-# Set build cache folder to customROM out dir
+  # Set build cache folder to customROM out dir
 
-sed -i 's|build_cache=/media/martin/extLinux/developer/android/cache/lineageOS/10|build_cache=/home/developer/android/rom/lineageOS/10/out|g' ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
+  sed -i 's|build_cache=/media/martin/extLinux/developer/android/cache/lineageOS/10|build_cache=/home/developer/android/rom/lineageOS/10/out|g' ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
 
-# Run build
+  # Set build folder for final rom output
 
-./build.sh
+  sed -i 's|build_out=/media/martin/extLinux/developer/android/out/lineageOS/10|build_out=/home/developer/out|g' ~/SCRIPTS_BUILD/ROM/SODP/XPERIA_TAMA/LineageOS/build.sh
+
+  # Run build
+
+  ./build.sh
+) |& tee ~/out/build.log
